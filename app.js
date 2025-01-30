5933ab509a9a362243c72a285adeac429fc18961
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-30 13:54:41 +0600

    centralized app entry point

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const productRoutes = require("./src/modules/product/product.route");
+const authRoutes = require("./src/modules/auth/auth.route");
+const userRoutes = require("./src/modules/user/user.route");
+app.use(express.json());
+const { DB } = require("./src/models/db");
+
+const corsOptions = {
+  origin: ["https://stock-plus-five.vercel.app", "http://localhost:3000",],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
+};
+
+// app.use(cors(corsOptions));
+app.use(cors())
+app.use("/api", productRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
