cf8299975c32f0c0cbc78b6fb9f0456c6b1649db
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-08 20:53:48 +0600

    added routes for revenue

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,45 +6,50 @@ const customerRoutes = require("../modules/customer/customer.route");
 const attributeRoutes = require("../modules/attribute/attribute.route");
 const transactionRoutes = require("../modules/transaction/transaction.route");
 const dashboardRoutes = require("../modules/dashboard/dashboard.route");
+const revenueRoutes = require("../modules/revenue/revenue.route");
 
 const router = require("express").Router();
 
 const routes = [
   {
     path: "/api",
-    routes: authRoutes
+    routes: authRoutes,
   },
   {
     path: "/api",
-    routes: userRoutes
+    routes: userRoutes,
   },
   {
     path: "/api",
-    routes: productRoutes
+    routes: productRoutes,
   },
   {
     path: "/api",
-    routes: invoiceRoutes
+    routes: invoiceRoutes,
   },
   {
     path: "/api",
-    routes: customerRoutes
+    routes: customerRoutes,
   },
   {
     path: "/api",
-    routes: attributeRoutes
+    routes: attributeRoutes,
   },
   {
     path: "/api",
-    routes: transactionRoutes
+    routes: transactionRoutes,
   },
   {
     path: "/api",
-    routes: dashboardRoutes
+    routes: dashboardRoutes,
   },
-]
+  {
+    path: "/api",
+    routes: revenueRoutes,
+  },
+];
 
-routes.forEach(route => {
+routes.forEach((route) => {
   router.use(route.path, route.routes);
 });
 
