2ab93c79e1f881ac4dc7fdd797b92951890c594b
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-09 21:57:35 +0600

    updated database

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,14 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x7pm4nr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x7pm4nr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.puy7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+MongoClient.connect(uri, function(err, client) {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -11,21 +19,15 @@ const client = new MongoClient(uri, {
 });
 
 const DB = client.db("StockManagerDB");
-const productCollection = DB.collection("products");
-const userCollection = DB.collection("users");
-const invoiceCollection = DB.collection("invoices");
-const customerCollection = DB.collection("customers");
-const attributeCollection = DB.collection("attributes");
-const transactionCollection = DB.collection("transactions");
-const revenueCollection = DB.collection("revenue");
+
 module.exports = {
   client,
   DB,
-  productCollection,
-  userCollection,
-  invoiceCollection,
-  customerCollection,
-  attributeCollection,
-  transactionCollection,
-  revenueCollection
+  productCollection: DB.collection("products"),
+  userCollection: DB.collection("users"),
+  invoiceCollection: DB.collection("invoices"),
+  customerCollection: DB.collection("customers"),
+  attributeCollection: DB.collection("attributes"),
+  transactionCollection: DB.collection("transactions"),
+  revenueCollection: DB.collection("revenue")
 };
