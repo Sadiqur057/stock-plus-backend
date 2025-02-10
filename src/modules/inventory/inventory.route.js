e603ac45e30682909731585ee20d66101316a778
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-10 15:11:49 +0600

    added routes for inventory add and delete

--- src/modules/inventory/inventory.route.js
+++ src/modules/inventory/inventory.route.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const { verifyToken } = require("../../middleware/authorization");
-const { addItems, getItems } = require("./inventory.controller");
+const {
+  addItems,
+  getItems,
+  getInventoryReport,
+  deleteInventoryReport,
+} = require("./inventory.controller");
 const router = express.Router();
 
 router.post("/add-items", verifyToken, addItems);
 router.get("/get-items", verifyToken, getItems);
+router.get("/inventory-report/:id", verifyToken, getInventoryReport);
+router.delete("/delete-report/:id", verifyToken, deleteInventoryReport);
 
 module.exports = router;
